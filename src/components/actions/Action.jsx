b01aa132b245cbc1338ec2f6f7b6baa39f9931e4
Zyad Abdul-Nasser <zizo_007@live.com>
2024-04-20 00:38:53 +0200

    z

--- src/components/actions/Action.jsx
+++ src/components/actions/Action.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Action({text, link}) {
     return (
-        <a href={"" + link} target='_blank' type="button" class="cursor-pointer mb-2.5 me-1.5 py-2 px-3 inline-flex justify-center items-center gap-x-2 rounded-lg border border-blue-600 bg-white text-blue-600 align-middle hover:bg-blue-50 text-sm dark:bg-neutral-900 dark:text-blue-500 dark:border-blue-500 dark:hover:text-blue-400 dark:hover:border-blue-400">
+        <a href={"" + link} target='_blank' type="button" class="cursor-pointer mb-2.5 me-1.5 py-2 px-3 inline-flex justify-center items-center gap-x-2 rounded-lg border border-blue-600 bg-white text-blue-600 align-middle hover:bg-blue-50 text-sm ">
             {text}
         </a>
     )
