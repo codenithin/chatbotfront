489296d827822e7551088a52094a135f5f8a38ab
Zyad Abdul-Nasser <zizo_007@live.com>
2024-04-24 20:46:36 +0200

    z

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3000"
-const BASE_URL = ''
-// const BASE_URL = 'https://triniti-chatbot.onrender.com'
+// const BASE_URL = ''
+const BASE_URL = 'https://triniti-chatbot.onrender.com'
 
 export default BASE_URL
